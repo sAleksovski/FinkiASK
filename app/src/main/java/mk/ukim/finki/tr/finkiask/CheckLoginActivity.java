a5d8cce2fb48a6fd2a5ecf8bce522b7f5eaebd37
Stefan Aleksovski <aleksovski.stefan994@gmail.com>
2015-07-28 01:29:19 +0200

    Initial commit

---
+++ app/src/main/java/mk/ukim/finki/tr/finkiask/CheckLoginActivity.java
@@ -0,0 +1,52 @@
+package mk.ukim.finki.tr.finkiask;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import mk.ukim.finki.tr.finkiask.helper.AuthHelper;
+
+public class CheckLoginActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if ( ! AuthHelper.isLoggedIn(this)) {
+            Intent intent = new Intent(this, LoginActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        } else {
+            Intent intent = new Intent(this, MainActivity.class);
+            intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            startActivity(intent);
+        }
+
+        finish();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_check_login, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
---
+++ app/src/main/java/mk/ukim/finki/tr/finkiask/helper/AuthHelper.java
@@ -0,0 +1,33 @@
+package mk.ukim.finki.tr.finkiask.helper;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.SharedPreferences;
+
+/**
+ * Created by stefan on 7/28/15.
+ */
+public class AuthHelper {
+
+    public static final String PREFS_NAME = "FINKI_ASK_PREFS";
+    public static final String SESSION_USER = "SESSION_USER";
+    public static final String SESSION_VALID_UNTIL = "SESSION_VALID_UNTIL";
+    private static final String SESSION_HASH = "SESSION_HASH";
+
+    public static boolean isLoggedIn(Activity activity) {
+
+        SharedPreferences settings = activity.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        long currentTime = System.currentTimeMillis();
+
+        String user = settings.getString(SESSION_USER, null);
+        long sessionValidUntil = settings.getLong(SESSION_VALID_UNTIL, -1);
+
+        if (user != null) {
+            return currentTime < sessionValidUntil;
+        } else {
+            return false;
+        }
+
+    }
+
+}
