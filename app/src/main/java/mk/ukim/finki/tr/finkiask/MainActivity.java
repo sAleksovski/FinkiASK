ad50af533e947ed96f7821e48682b738bcda4f67
Stefan Aleksovski <aleksovski.stefan994@gmail.com>
2015-07-31 22:01:31 +0200

    Fixed masterdetail view

--- app/src/main/java/mk/ukim/finki/tr/finkiask/MainActivity.java
+++ app/src/main/java/mk/ukim/finki/tr/finkiask/MainActivity.java
@@ -1,14 +1,14 @@
 package mk.ukim.finki.tr.finkiask;
 
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.LinearLayoutManager;
 
 import java.util.ArrayList;
 
